38a85c0eeeba17875d856e43e22db143230d810d
hh22035@ues.edu.sv <kike@Kike>
2024-05-28 00:43:35 -0600

    El menu le pertenece a Gestor de ventas

--- erp/ventas/urls.py
+++ erp/ventas/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('listadoServicio/', views.listadoServicio_View, name='listadoServicio'),
     path('nuevoServicio/', views.newService_View, name = 'nuevoServicio'),
-    path('registroVentas/', views.registroVentas_View, name='registroVentas')
+    path('registroVentas/', views.registroVentas_View, name='registroVentas'),
+    path('menuGestor/', views.menuGestor_View, name='menuGestor')
 ]
--- erp/ventas/views.py
+++ erp/ventas/views.py
@@ -26,3 +26,6 @@ def newService_View(request):
 
 def registroVentas_View(request):
     return render(request, 'registroVentas.html')
+
+def menuGestor_View(request):
+    return render(request, 'menuGestor.html')
